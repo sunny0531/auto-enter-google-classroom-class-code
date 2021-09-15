289cc5b9ab76b7871ec7fa75fca0861f558229f7
Sunny <sunny.ayyl@gmail.com>
2021-09-15 17:26:18 +0100

    Fix and add log(kind of)

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ with open("classcode.txt") as f:
 
 def click(image,alternate=None):
     while True:
+        print("Trying to click " + image)
         if pyautogui.locateOnScreen(image) is not None:
             pyautogui.click(image)
+            print("Clicked "+image)
             break
         elif alternate is not None and pyautogui.locateOnScreen(alternate):
             pyautogui.click(alternate)
+            print("Clicked " + image)
             break
         time.sleep(0.5)
     time.sleep(0.5)
 for i in class_code_list:
     time.sleep(1)
     click("images/add.png")
-    click("images/join_class.png")
-    time.sleep(0.3)
-    pyautogui.press("tab")
-    pyautogui.press("tab")
+    #click("images/join_class.png") #I am on student account
+    #time.sleep(0.3)
+    click("images/img_2.png")
     pyautogui.write(i)
     time.sleep(1.5)
     click("images/button.png")
